a4605e6dbb5d0202b41e8b5eaa783f7900f1a380
Roman Rudenko <ruroma18@gmail.com>
2022-05-10 20:51:17 +0300

    Add formik task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import "./App.css";
 import "./reset.css";
-import { Component } from "react";
-import { UserContext } from "./contexts";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 
-class App extends Component {
-  state = {
-    user: {
-      id: 1,
-      login: 'testTest',
-      email: 'test@test.com',
-      imgSrc: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    },
-  };
-  
-  render() {
-    return (
-      <UserContext.Provider value={this.state.user}>
-        <Header />
-        <Sidebar />
-      </UserContext.Provider>
-      
-    );
-  }
+import React from 'react';
+import SignInPage from "./pages/SignIn";
+
+const App = () => {
+  return (
+    <div>
+      <SignInPage />      
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignInForm from '../../components/SignInForm';
+
+const SignInPage = () => {
+  return (
+    <>
+    <h1 style={{margin: '0 auto', fontSize: '1.5em'}}>Sign Up</h1>
+    <SignInForm />
+    </>
+    
+  );
+}
+
+export default SignInPage;
